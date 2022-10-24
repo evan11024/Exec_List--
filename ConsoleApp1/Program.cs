b48863197acb3bd66726c34b05ec26db4a7b269b
ispan <ispan@DESKTOP-PAKSETB>
2022-10-24 16:54:59 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Product> products = new List<Product>()
+			{
+				new Product{ Name = "water ",Price = 10 },
+				new Product{Name = "beer",Price = 30 },
+			};
+		}
+	}
+	class Product
+	{
+		public string Name { get; set; }
+		public int Price { get; set; }
+
+	}
+}
